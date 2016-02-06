7faca87e22706b337cd10d6247b808f10f8fc8ab
Sidney Ng <yendisng@gmail.com>
2016-02-06 13:58:22 -0500

    ssd, but what of the border?

--- Assignment1/Assignment1.m
+++ Assignment1/Assignment1.m
@@ -18,8 +18,8 @@ templateFileNames = dir('Template images/Template-*.png');
 numTemplates = length(templateFileNames);
 
 %% Set the values of SSD_THRESH and NCC_THRESH
-SSD_THRESH = 10;
-NCC_THRESH = 10;
+SSD_THRESH = 50000;
+NCC_THRESH = 50000;
 
 %% Initialize two output images to the RGB input image
 output_img1 = image;
@@ -28,8 +28,8 @@ output_img2 = image;
 %% For each template, do the following
 for i=1:numTemplates
     %% Load the RGB template image, into variable T
-    path = sprintf('Template images/%s', templateFileNames(i).name);
-    T = imread(path);
+    filepath = sprintf('Template images/%s', templateFileNames(i).name);
+    T = imread(filepath);
     
     %% Convert the template to gray-scale
     T = rgb2gray(T);
@@ -79,23 +79,70 @@ function [SSDrow, SSDcol] = SSD(grayImage, T, SSD_THRESH)
 % first step is to compute the SSD matrix
 T_row = size(T,1);
 T_col = size(T,2);
+half_Tr = idivide(T_row, int32(2));
+half_Tc = idivide(T_col, int32(2));
 Gray_row = size(grayImage,1);
 Gray_col = size(grayImage,2);
-SSD_matrix = zeros(Gray_row, Gray_col);
+SSD_matrix = zeros(Gray_row-T_row, Gray_col-T_col);
+min_ssd = realmax;
+min_row = 0;
+min_col = 0;
 % assume that the template is within the image bounds (reasonable?)
-for row = 1:(Gray_row)
-    for col = 1:(Gray_col)
-        row_offset = 0;
-        col_offset = 0;
-        squared_diff = (T - grayImage(row:row+T_row-1, col:col+T_col-1)).^2;
+% for row = 1:(Gray_row)
+%     for col = 1:(Gray_col)
+%         % make sure that image size is not exceeded
+%         % adjust template size for boundaries
+%         TR = T_row;
+%         TC = T_col;
+%         row_offset = row + T_row-1;
+%         col_offset = col + T_col-1;
+%         if (row + T_row-1) > Gray_row
+%             row_offset = Gray_row;
+%             TR = T_row - (row+T_row-1-Gray_row);
+%         end
+%         if (col + T_col-1) > Gray_col
+%             col_offset = Gray_col;
+%             TC = T_col - (col+T_col-1-Gray_col);
+%         end
+%         
+%         squared_diff = (T(1:TR, 1:TC) - grayImage(row:row_offset, col:col_offset)).^2;
+%         ssd = sum(sum(squared_diff));
+%         SSD_matrix(row, col) = ssd;
+%     end
+% end
+for row = half_Tr:(Gray_row-half_Tr-1)
+    for col = half_Tc:(Gray_col-half_Tc-1)
+        patch = grayImage(row-half_Tr+1:row+half_Tr+1, col-half_Tc+1:col+half_Tc+1);
+        squared_diff = (T - patch).^2;
         ssd = sum(sum(squared_diff));
-        SSD_matrix(row, col) = ssd;
+        if ssd < min_ssd
+           min_ssd = ssd;
+           min_row = row;
+           min_col = col;
+        end
+        SSD_matrix(row-half_Tr+1, col-half_Tc+1) = ssd;
     end
 end
+
     disp(mean(mean(SSD_matrix)));
     disp(max(max(SSD_matrix)));
-    disp(min(min(SSD_matrix)));
-    imshow(SSD_matrix);
+    k = min(min(SSD_matrix));
+    index = find(SSD_matrix==k);
+    if (k < SSD_THRESH)
+       disp(k);
+       disp(index);
+    end
+    SSDrow = -1;
+    SSDcol = -1;
+    if (min_ssd < SSD_THRESH)
+       disp(min_ssd);
+       disp(min_row);
+       disp(min_col);
+       
+       SSDrow = min_row;
+       SSDcol = min_col;
+    end
+%     imshow(SSD_matrix);
 
 end
 
