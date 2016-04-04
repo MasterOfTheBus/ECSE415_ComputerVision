220c127d0184e5190a6c8ca31fb67323b816f15a
Sidney Ng <yendisng@gmail.com>
2016-04-04 14:47:08 -0400

    no more exceptions; need to not get a blank mask

--- Assignment4/Assignment4/main.cpp
+++ Assignment4/Assignment4/main.cpp
@@ -4,7 +4,6 @@
 #include <vector>
 #include "stdlib.h"
 
-#define DEBUGGING_KMEANS 0
 #define INTENSITY 0
 
 using namespace std;
@@ -15,7 +14,7 @@ Mat calc_gmm(Mat img, int rect[]);
 Mat calc_graphCut(Mat img, int rect[]);
 Mat maskImg(Mat img, Mat mask);
 
-int main(/*int argc, char *argv[]*/)
+int main()
 {
     const string base_dir = "C:\\Users\\Lazy\\Documents\\GitHub\\Computer_Vision\\Assignments\\Assignment4\\";
     string image_names[] = {base_dir + "100_0109.png",
@@ -53,12 +52,12 @@ int main(/*int argc, char *argv[]*/)
             waitKey(0);
             destroyWindow("kmeans");
 
-//            Mat gmm_mask = calc_gmm(img, rect_points);
-//            out_img = maskImg(img, gmm_mask);
-//            // display the results
-//            imshow("gmm", out_img);
-//            waitKey(0);
-//            destroyWindow("gmm");
+            Mat gmm_mask = calc_gmm(img, rect_points);
+            out_img = maskImg(img, gmm_mask);
+            // display the results
+            imshow("gmm", out_img);
+            waitKey(0);
+            destroyWindow("gmm");
 
             Mat graphCut_mask = calc_graphCut(img, rect_points);
             out_img = maskImg(img, graphCut_mask);
@@ -83,21 +82,19 @@ Mat calc_kmeans(Mat img, int rect[]) {
     // create the corresponding user drawn rect
     Rect_<double> user_rect(rect[0], rect[1], rect[2], rect[3]);
 
-#if DEBUGGING_KMEANS
-    CvScalar color = {0.0, 0.0, 255.0, 0.0};
-    Mat outIm = img;
-    rectangle(outIm, user_rect, color);
-    imshow("img with rect", outIm);
-    waitKey(0);
-    destroyWindow("img with rect");
-#endif
-
     // feature matrix
 //    Mat feature_mat = img.reshape(1, H*W);
+    Mat feature_mat;
 #if INTENSITY
+    feature_mat = Mat::zeros(H*W, 1, CV_32F);
     cvtColor(img, img, CV_RGB2GRAY);
-#endif
-    Mat feature_mat = Mat::zeros(H*W, 3, CV_32F);
+    for (int col = 0; col < W; col++) {
+        for (int row = 0; row < H; row++) {
+            feature_mat.at<float>(col*H+row,0) = img.at<float>(row, col);
+        }
+    }
+#else
+    feature_mat = Mat::zeros(H*W, 3, CV_32F);
     for (int col = 0; col < W; col++) {
         for (int row = 0; row < H; row++) {
             feature_mat.at<float>(col*H+row,0) = img.at<Vec3f>(row, col)[2]; // red
@@ -105,6 +102,7 @@ Mat calc_kmeans(Mat img, int rect[]) {
             feature_mat.at<float>(col*H+row,2) = img.at<Vec3f>(row, col)[0]; // blue
         }
     }
+#endif
 
     // initial label matrix that has points assigned for inside and outside
     Mat init_label = Mat::zeros(H*W, 1, CV_32S);
@@ -114,35 +112,11 @@ Mat calc_kmeans(Mat img, int rect[]) {
         }
     }
 
-#if DEBUGGING_KMEANS
-    Mat label_out = Mat::zeros(H, W, CV_8U);
-    for (int i = 0; i < W; i++) {
-        for (int j = 0; j < H; j++) {
-            label_out.at<char>(j, i) = 255*init_label.at<int>(i*H+j, 0);
-        }
-    }
-    imshow("label", label_out);
-    waitKey(0);
-    destroyWindow("label");
-#endif
-
     // run kmeans
     int k = 2, attempts = 3;
     kmeans(feature_mat, k, init_label,
            TermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 1000, 0.0001),
            attempts, KMEANS_USE_INITIAL_LABELS);
-
-#if DEBUGGING_KMEANS
-    label_out = Mat::zeros(H, W, CV_8U);
-    for (int i = 0; i < W; i++) {
-        for (int j = 0; j < H; j++) {
-            label_out.at<char>(j, i) = 255*init_label.at<int>(i*H+j, 0);
-        }
-    }
-    imshow("label after convergence", label_out);
-    waitKey(0);
-    destroyWindow("label after convergence");
-#endif
 
     // create the foreground mask matrix
     // TODO: use reshape here and above
@@ -159,47 +133,61 @@ Mat calc_kmeans(Mat img, int rect[]) {
 Mat calc_gmm(Mat img, int rect[]) {
     int H = img.size().height;
     int W = img.size().width;
+    int clusters = 2, dim = 3;
 
     // create the corresponding user drawn rect
     Rect_<double> user_rect(rect[0], rect[1], rect[2], rect[3]);
 
 #if INTENSITY
-    cvtColor(img, img, CV_RGB2Lab);
+    cvtColor(img, img, CV_RGB2GRAY);
 #endif
 
+    img.convertTo(img, CV_64F);
+
     // Create Expectation Maximization object, feature matrix, initial means matrix
-    EM em(2);
+    EM em(clusters, EM::COV_MAT_DIAGONAL, TermCriteria(CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 1000, 0.0001));
     Mat feature_mat = img.reshape(1, H*W);
-    feature_mat.convertTo(feature_mat, CV_64F);
-    Mat initial_means = Mat::zeros(2, 3, CV_64F);
+    Mat initial_means = Mat::zeros(clusters, dim, CV_64F);
 
     // Initial covariation matrices
     vector<Mat> covar;
-    covar.push_back(Mat::zeros(2,2,CV_64F));
-    covar.push_back(Mat::zeros(2,2,CV_64F));
+    covar.push_back(Mat::zeros(dim,dim,CV_64F));
+    covar.push_back(Mat::zeros(dim,dim,CV_64F));
 
-    Mat inbox, outbox;
-    outbox.push_back((img.rowRange(0, user_rect.y)).reshape(0, W*user_rect.y));
-    outbox.push_back((img.rowRange(user_rect.y+user_rect.height, H)).reshape(0, W*(H-user_rect.height-user_rect.y)));
+    Mat outbox;
+    outbox.push_back(img.rowRange(0, user_rect.y));
+    outbox = outbox.reshape(0, user_rect.y * W);
 
-//    for (int row = 0; row < H; row++) {
-//        for (int col = 0; col < W; col++) {
-//            if () {
+    for (int row = user_rect.y; row < user_rect.y+user_rect.height; row++) {
+        for (int col = 0; col < W; col++) {
+            if ((col < user_rect.x || col > user_rect.x+user_rect.width)) {
+                outbox.push_back(img.at<Vec3d>(row, col));
+            }
+        }
+    }
+    Mat last_rows = img.rowRange(user_rect.y+user_rect.height, H);
+    outbox.push_back(last_rows.reshape(0, (H-user_rect.y-user_rect.height) * W));
+    outbox = outbox.reshape(1, 0);
 
-//            }
-//        }
-//    }
+    Mat inbox_submat = img(user_rect);
+    Mat inbox = inbox_submat.clone(); // make sure that the data is continuous before reshaping
+    inbox = inbox.reshape(1, inbox.rows * inbox.cols); // reshape so that we can pass to calcCovarMatrix
 
     calcCovarMatrix(inbox, covar[0], initial_means.row(0), CV_COVAR_NORMAL | CV_COVAR_ROWS);
     calcCovarMatrix(outbox, covar[1], initial_means.row(1), CV_COVAR_NORMAL | CV_COVAR_ROWS);
 
     // Initial weight
-    Mat init_weight = Mat::zeros(1, 2, CV_32F);
+    Mat init_weight = Mat::zeros(1, clusters, CV_32F);
     init_weight.at<float>(0,0) = user_rect.area() / (H*W);
     init_weight.at<float>(0,1) = 1 - init_weight.at<float>(0,0);
     Mat labels;
 
     em.trainE(feature_mat, initial_means, covar, init_weight, noArray(), labels);
+
+    // reshape the labels so that it's an H * W matrix
+    labels = labels.reshape(0, H);
+    labels.convertTo(labels, CV_8UC1);
+    cout << "labels size: " << labels.size() << endl;
 
     return labels;
 }
@@ -211,7 +199,7 @@ Mat calc_graphCut(Mat img, int rect[]) {
     Mat mask, bgdModel, fgdModel;
 
 #if INTENSITY
-    cvtColor(img, img, CV_RGB2GRAY);
+    cvtColor(img, img, CV_RGB2Lab);
 #endif
 
     grabCut(img, mask, user_rect, bgdModel, fgdModel, 3, GC_INIT_WITH_RECT);
